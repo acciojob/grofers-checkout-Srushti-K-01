3fda4e6bc174dacf5fe6586ace53cc0b8a886505
Srushti-K-01 <ksrushtimahesh@gmail.com>
2025-02-17 19:00:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-// const getSumBtn = document.createElement("button");
-// getSumBtn.append("Get Total Price");
-// document.body.appendChild(getSumBtn);
-
-// const getSum = () => {
-// //Add your code here
-// let price = document.querySelectorAll(".price");
-// 	let sum =0;
-// 	price.forEach((price)=>{
-// 	sum = sum + parseInt(price.textContent);
-// 	})
-// 	let newRowExist = document.getElementById("ans");
-	
-// 	if(!newRowExist){
-// 	let newRow = document.createElement("tr");
-// 	newRow.id=ans;
-// 	let newCell = document.createElement("td");
-// 	newCell.textContent = (`Total Price = ${sum}`);
-// 	newRow.append(newCell);
-// 	document.querySelector("table").append(newRow);
-// 	}
-// };
-
-// getSumBtn.addEventListener("click", getSum);
-
-
-
 // Create a button dynamically
 const getSumBtn = document.createElement("button");
 getSumBtn.textContent = "Get Total Price";
